5890f0cf5f474d2871f1a6f87223e4e929ec8b99
PhilKunz <github@philkunz.com>
2016-01-25 05:01:20 +0100

    more cleanp

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/// <reference path="ts/typings/tsd.d.ts" />
+declare module AppmanagerGulp {
+    function init(): void;
+}
+declare module AppmanagerNginx {
+}
+declare var gulp: any;
+declare var path: any;
+declare var pr: any;
+declare var appmanager: any;
